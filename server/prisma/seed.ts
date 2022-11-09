a854d8e929767eab3aa5e25a68260121fbf0f7bd
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-11-09 23:49:52 +0200

    Create database seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      avatarUrl: "https://github.com/jose-bone.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example Pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-10T16:00:00.201Z",
+      firstTeamCountryCode: "AO",
+      secondTeamCountryCode: "MZ",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-11T16:00:00.201Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoint: 2,
+          secondTeamPoint: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
